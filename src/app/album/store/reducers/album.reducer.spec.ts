f7df1555a9315fdc2ae23f3e519a4549bc2d53d5
rijine <e.rijin@gmail.com>
2018-02-19 13:12:32 +0530

    initial state for unittest

--- src/app/album/store/reducers/album.reducer.spec.ts
+++ src/app/album/store/reducers/album.reducer.spec.ts
@@ -1,13 +1,13 @@
-import { reducer, initialState } from '../reducers/album.reducer';
+import { reducer, initialAlbumsState } from '../reducers/album.reducer';
 
 describe('Album Reducer', () => {
   describe('unknown action', () => {
     it('should return the initial state', () => {
       const action = {} as any;
 
-      const result = reducer(initialState, action);
+      const result = reducer(initialAlbumsState, action);
 
-      expect(result).toBe(initialState);
+      expect(result).toBe(initialAlbumsState);
     });
   });
 });
